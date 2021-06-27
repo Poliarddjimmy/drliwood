80d2bf0fa7bde997952c60d552af64788745bd80
Poliard Djimmy <poliarddjimmy@gmail.com>
2021-06-27 16:06:17 -0400

    Added assets, update styles

---
+++ pages/tracks.js
@@ -0,0 +1,23 @@
+// import imgSrc from "./assets/artwork.jpg";
+// import imgSrc2 from "./assets/artwork2.jpg";
+
+// import cali from "./example.mp3";
+// import fifty from "./exo1.mp3";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+  {
+    title: "Cali",
+    artist: "Wataboi",
+    audioSrc: "/musics/example.mp3",
+    image: '/assets/artwork.jpg',
+    color: "#00aeb0"
+  },
+  {
+    title: "50",
+    artist: "tobylane",
+    audioSrc: "/musics/exo1.mp3",
+    image: "/assets/artwork2.jpg",
+    color: "#ffb77a"
+  },
+];
---
+++ public/musics/tracks.js
@@ -0,0 +1,23 @@
+import imgSrc from "./assets/artwork.jpg";
+import imgSrc2 from "./assets/artwork2.jpg";
+
+import cali from "./example.mp3";
+import fifty from "./exo1.mp3";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+  {
+    title: "Cali",
+    artist: "Wataboi",
+    audioSrc: cali,
+    image: imgSrc,
+    color: "#00aeb0"
+  },
+  {
+    title: "50",
+    artist: "tobylane",
+    audioSrc: fifty,
+    image: imgSrc2,
+    color: "#ffb77a"
+  },
+];
