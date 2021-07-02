03fe1c80b526d49e2dd5e16e9a4a643024293b69
Poliard Djimmy <poliarddjimmy@gmail.com>
2021-07-01 20:57:30 -0400

    build sales pages

---
+++ pages/sales.js
@@ -0,0 +1,25 @@
+import Layout from "../components/layouts/layout";
+import Image from "next/image";
+
+const Sales = () => {
+  return <Layout>
+    <section className="banner-title album-banner" style={{ backgroundP: "50% 0px" }}>
+      <div className="overlay"></div>
+      <div className="container content">
+        <div className="col-md-12 col-sm-12 col-xs-12">
+          <div className="main-title">
+            <h1>
+              Sales
+            </h1>
+            <div className="text-wrap">
+              <div className="text">Your custom text here.</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+  </Layout>
+}
+
+export default Sales;
